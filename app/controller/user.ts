32a433e4dc738426fc80b7d5aa92f5a23ae7e326
yolandayuhui <1176794277@qq.com>
2021-04-03 00:38:18 +0800

    不同类型的注册数据的校验

--- app/controller/user.ts
+++ app/controller/user.ts
@@ -1,13 +1,42 @@
 import { Controller } from 'egg';
 import NormalUserRule from '../validate/NormalUserRule'
+import EmailUserRule from '../validate/EmailUserRule'
+import PhoneUserRule from '../validate/PhoneUserRule'
+const enum RegisterTypeEnum {
+  Phone = "phone",
+  Email = "email",
+  Normal = "normal"
+}
 export default class UserController extends Controller {
-  public async register() {
+  public async create() {
     const { ctx } = this;
     try {
-      ctx.validate(NormalUserRule, ctx.request.body);
-      ctx.body = "注册2"
+      this.validateUserInfo();
+      ctx.body = "注册"
     } catch (e) {
       console.log(e)
+      if (e.errors) {
+        ctx.body = e.errors;
+      } else {
+        ctx.body = e.message;
+      }
+    }
+  }
+  private validateUserInfo() {
+    const { ctx } = this;
+    const data = ctx.request.body
+    switch (data.registerType) {
+      case RegisterTypeEnum.Email:
+        ctx.validate(EmailUserRule, data);
+        break;
+      case RegisterTypeEnum.Phone:
+        ctx.validate(PhoneUserRule, data);
+        break;
+      case RegisterTypeEnum.Normal:
+        ctx.validate(NormalUserRule, data);
+        break;
+      default:
+        throw new Error("不存在此类型")
     }
   }
 }
--- app/validate/NormalUserRule.ts
+++ app/validate/NormalUserRule.ts
@@ -1,8 +1,27 @@
 export default {
     username: {
         type: 'string',
-        format: /^[0-9a-zA-Z]{6,}$/, 
-        required: true,
-        message:"请输入6位以上的数字或字母"
+        trim: true,
+        // 只能是数字或字母
+        format: /^[A-Za-z0-9]{6,}$/,
+        message: '用户名不符合要求'
+    },
+    password: {
+        type: 'string',
+        trim: true,
+        // 必须是数字字母符号组合
+        format: /^(?:(?=.*[0-9].*)(?=.*[A-Za-z].*)(?=.*[,\.#%'\+\*\-:;^_`].*))[,\.#%'\+\*\-:;^_`0-9A-Za-z]{8,}$/,
+        message: '密码不符合要求'
+    },
+    captcha: {
+        type: 'string',
+        trim: true,
+        // 必须是数字字母符号组合
+        format: /^[A-Za-z0-9]{4}$/,
+        message: '验证码不符合要求'
+    },
+    registerType: {
+        type: 'enum',
+        values: ['normal', 'email', 'phone']
     }
 }
